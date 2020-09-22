d82209aa87b4568de546d54b42a184a87b3d6ee7
Giovanni Bussi <giovanni.bussi@gmail.com>
2020-09-22 19:58:25 +0200

    Enforce clang format
    Cmd: CLANG_FORMAT=clang-format-mp-7.0 ./admin/reformat_all.sh

--- src/gromacs/utility/pleasecite.cpp
+++ src/gromacs/utility/pleasecite.cpp
@@ -294,7 +294,8 @@ void please_cite(FILE* fp, const char* key)
           "Accelerated weight histogram method for exploring free energy landscapes",
           "J. Chem. Phys.", 141, 2014, "044110" },
         { "Bernetti2020", "M. Bernetti, G. Bussi",
-          "Pressure control using stochastic cell rescaling", "J. Chem. Phys.", 153, 2020, "114107" },
+          "Pressure control using stochastic cell rescaling", "J. Chem. Phys.", 153, 2020,
+          "114107" },
     };
 #define NSTR static_cast<int>(asize(citedb))
 
